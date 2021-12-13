e08a8178c85565e1152134c63c76025cf76de746
svetlanagaydey <lana84@ukr.net>
2021-12-13 13:58:12 +0200

    class component refactor completed

--- project/src/4.2-cards/CardsComponent.jsx
+++ project/src/4.2-cards/CardsComponent.jsx
@@ -1,23 +1,26 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import Card from './Card.jsx'
 
 
 
-const CardsComponent = () => {
-    const names = ['Tom', 'Sam', 'Will'];
-    const articles = [
-        'Lorem that ipsum, dolor sit amet consectetur adipisicing elit.',
-        'Hello dolor, sit amet consectetur adipisicing elit.',
-        'Seems ipsum dolor, sit amet consectetur adipisicing elit.'
-    ]
-   
-    return (
-        <div>
-            <Card imageUrl ="https://picsum.photos/370/140" name ={names[0]} article ={articles[0]}/>
-            <Card imageUrl ="https://picsum.photos/371/140" name ={names[1]} article ={articles[1]}/>
-            <Card imageUrl ="https://picsum.photos/372/140" name ={names[2]} article ={articles[2]}/>
-        </div>
-    )
+class CardsComponent extends React.Component {
+    render () {
+        const names = ['Tom', 'Sam', 'Will'];
+        const articles = [
+            'Lorem that ipsum, dolor sit amet consectetur adipisicing elit.',
+            'Hello dolor, sit amet consectetur adipisicing elit.',
+            'Seems ipsum dolor, sit amet consectetur adipisicing elit.'
+        ]
+    
+        return (
+            <div>
+                <Card imageUrl ="https://picsum.photos/370/140" name ={names[0]} article ={articles[0]}/>
+                <Card imageUrl ="https://picsum.photos/371/140" name ={names[1]} article ={articles[1]}/>
+                <Card imageUrl ="https://picsum.photos/372/140" name ={names[2]} article ={articles[2]}/>
+            </div>
+        )
+    }
 }
 
 
